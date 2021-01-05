d34034e5d516ba29574c0518c81bf125ff0869e5
Patrik Sylve <patriksylve@pop-os.localdomain>
2021-01-05 20:38:14 +0100

    board set up fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     NgxChessBoardModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -9,10 +9,29 @@ import { NgxChessBoardService, NgxChessBoardView } from 'ngx-chess-board';
 export class BoardComponent implements OnInit {
 
   @ViewChild('board', { static: false }) board: NgxChessBoardView | undefined;
+  boardSize = 800;
+
+  // Blueish theme
+  blackColor = "#8ea2ac";
+  whiteColor = "#dee2e6";
 
   constructor(private ngxChessBoardService: NgxChessBoardService) { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Undo move
+   */
+  undo(): void {
+
+  }
+
+  /**
+   * Redo move
+   */
+
+  redo(): void {
+
+  }
 }
