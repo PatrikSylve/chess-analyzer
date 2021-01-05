828080d7f93b043c32764135a2507f18b69cc6b1
Patrik Sylve <patriksylve@pop-os.localdomain>
2021-01-05 20:08:34 +0100

    Setup chess board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxChessBoardModule,
+    NgxChessBoardModule.forRoot(),
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgxChessBoardService, NgxChessBoardView } from 'ngx-chess-board';
 
 @Component({
   selector: 'app-board',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('board', { static: false }) board: NgxChessBoardView | undefined;
+
+  constructor(private ngxChessBoardService: NgxChessBoardService) { }
 
   ngOnInit(): void {
   }
